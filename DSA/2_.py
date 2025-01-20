8547898bf93f41273f10c5420a5d64eb8baa0dbb
ayshajaved <ayshajavd@gmail.com>
2025-01-20 19:44:19 +0500

    Order of n

--- DSA/2_.py
+++ DSA/2_.py
@@ -1,4 +1,5 @@
 #Pratical/Experimental study
+import sys
 from time import time
 import tracemalloc as tm
 #Solution 1           #this is the best solution according to time and space
@@ -19,3 +20,8 @@ print(tm.get_tracemalloc_memory())
 tm.stop()
 end_time = time()
 print(end_time - start_time)
+
+#In python we can getsizeof() function to get the size of any data type
+print(sys.getsizeof(sumOf(10))) 
+#but it is not accurate because it does not take into account the memory used by the variables and functions
+#So we use tracemalloc to get the memory used by the program

eb583ad4095cc1fc8bee6c3420105861b6c320cb
ayshajaved <ayshajavd@gmail.com>
2025-02-22 22:08:23 +0500

    Sorting Array

--- ClassNotes/ArrayModule.py
+++ ClassNotes/ArrayModule.py
@@ -44,17 +44,35 @@ class ArrayOperations():
                 print("Value found at index", i)
                 return
         print("Value not found")
+    '''
+    There are two methods to sort, First by sorted(), it returns a new sorted list that is then converted to array, memory is used
+    Second by sort(), it sorts the array in place
+    so the memory is saved in the second method
+    '''
+    # def sortArray(self):
+        # self.arr =ar.array('i',sorted(self.arr))
+    # def sortArray(self):
+    #     self.arr = self.arr.tolist()
+    #     self.arr.sort() #pass reverse as true for descending order
+    #Sorting without using inbuilt functions
     def sortArray(self):
-        self.arr.tolist()
+        for i in range(0, len(self.arr)):
+            for j in range(i+1, len(self.arr)):
+                if self.arr[i] > self.arr[j]:
+                    self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
+    def reverseArray(self):
+        self.arr = self.arr[::-1]
 
 #Array operations
 obj = ArrayOperations()
-obj.createArray([1, 2, 3, 4, 5])
+obj.createArray([100, 23, 13, 42, 54])
 obj.addAtEnd(6)
 obj.addAtIndex(1,100)
 obj.displayArray()
 obj.deleteAtEnd()
 obj.displayArray()
 obj.deleteAtIndex(1)
+obj.displayArray()
+obj.sortArray()
 obj.displayArray()
 

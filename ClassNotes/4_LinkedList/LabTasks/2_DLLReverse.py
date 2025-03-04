e0cc0ee9912d29eae31875123158da02fec6176c
ayshajaved <ayshajavd@gmail.com>
2025-03-04 15:53:35 +0500

    Reversing of SLL and DLL

--- ClassNotes/4_LinkedList/LabTasks/2_DLLReverse.py
+++ ClassNotes/4_LinkedList/LabTasks/2_DLLReverse.py
@@ -1,72 +1,88 @@
-#Node class
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
         self.previous = None
-#Double Linked List class
+
 class DLL:
     def __init__(self):
-        #Initially the head of the Double Linked List is None
         self.head = None
 
     def length(self):
         count = 0
         currentNode = self.head
-        while(currentNode is not None):
-            count+=1
+        while currentNode is not None:
+            count += 1
             currentNode = currentNode.next
         return count
-    
+
     def appendNode(self, data):
         n = Node(data)
         if self.head is None:
             self.head = n
         else:
             currentNode = self.head
-            while(currentNode.next is not None):
+            while currentNode.next is not None:
                 currentNode = currentNode.next
             currentNode.next = n
             n.previous = currentNode
 
-    #Adding node at any index
     def addNode(self, at, data):
-        if(at < 0 or at > self.length()):
-            print("Enter Valid Index..")
+        if at < 0 or at > self.length():
+            print("Enter a valid index..")
             return
-        elif(at ==0):
-            node = Node(data)
-            self.head.previous = node
-            node.next = self.head
-            self.head = node
-        else:
-            n = Node(data)
-            currentNode = self.head
-            for i in range(at-1):
-                currentNode = currentNode.next
-            n.next = currentNode.next
-            n.previous = currentNode
-            currentNode.next = n
+        n = Node(data)
+        # Case 1: Adding at the head (index 0)
+        if at == 0:
+            if self.head is None:
+                self.head = n
+            else:
+                n.next = self.head
+                self.head.previous = n
+                self.head = n
+            return
+
+        # Case 2: Adding elsewhere in the list
+        currentNode = self.head
+        for _ in range(at - 1):
+            currentNode = currentNode.next
+
+        n.next = currentNode.next
+        n.previous = currentNode
+        if currentNode.next:
+            currentNode.next.previous = n
+        currentNode.next = n
 
     def displayList(self):
         currentNode = self.head
-        while(currentNode is not None):
-            print(currentNode.data)
+        while currentNode is not None:
+            print(currentNode.data, end=" ")
             currentNode = currentNode.next
-    
+        print()
+
     def deleteNode(self, index):
-        if (index <0 or index > (self.length())):
+        if index < 0 or index >= self.length():
             print("Index is invalid..")
             return
+        
+        # Case 1: Deleting the head node
+        if index == 0:
+            if self.head.next:
+                self.head = self.head.next
+                self.head.previous = None
+            else:
+                self.head = None  # List becomes empty
+            return
+
         currentNode = self.head
-        for _ in range(index-1):
+        for _ in range(index - 1):
             currentNode = currentNode.next
-        currentNode.next.previous = currentNode
-        currentNode.next = currentNode.next.next
-    
+        targetNode = currentNode.next
+        currentNode.next = targetNode.next
+        if targetNode.next:
+            targetNode.next.previous = currentNode
+
     def reverse(self):
-        '''Double linked list reversal is easy because we 
-        just have to exchange the next and prev of the nodes'''
         currentNode = self.head
         temp = None
         while currentNode:
@@ -76,8 +92,12 @@ class DLL:
             currentNode = currentNode.previous
         if temp:
             self.head = temp.previous
+
+# Testing the corrected code
 d = DLL()
-d.addNode(0,1)
-d.addNode(1,2)
-d.addNode(2,3)
+d.addNode(0, 1)
+d.addNode(1, 2)
+d.addNode(2, 3)
+d.displayList() 
+d.reverse()
 d.displayList()

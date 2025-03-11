7418d7ea0f8f52f81acdad90827952ea8b0a2573
ayshajaved <ayshajavd@gmail.com>
2025-03-11 23:07:45 +0500

    Inorder and pre order traversal done

--- ClassNotes/7_Trees/3_BinarytreeCode.py
+++ ClassNotes/7_Trees/3_BinarytreeCode.py
@@ -8,15 +8,52 @@ node creation, insertion, traversal (inorder, preorder, postorder), and searchin
 - preorder (Root, Left, Right)
 '''
 #Tree Node is defined for the binary trees
-class treeNode:
-    def __init__(self, value):
+class BinaryTree:
+    def __init__(self, value, left= None, right= None):
+        #initializing the root node
         self.data = value
         self.left = None
         self.right = None
-
-class BinaryTree:
-    def __init__(self, root_value):
-        self.root = treeNode(root_value)
     #method to insert the node
     def addNode(self,value):
-        pass
+        if self.data:
+            if value < self.data:
+                if not self.left:
+                    self.left = BinaryTree(value=value)
+                else:
+                    self.left.addNode(value)
+            else:
+                if not self.right:
+                    self.right = BinaryTree(value=value)
+                else:
+                    self.right.addNode(value=value)
+        else:
+            self.data = value
+    def inorder_traversal(self):
+        if self.left:
+            self.left.inorder_traversal()
+        print(self.data, end=" ")
+        if self.right:
+            self.right.inorder_traversal()
+    def preorder_traversal(self):
+        print(self.data, end=" ")
+        if self.left:
+            self.left.preorder_traversal()
+        if self.right:
+            self.right.preorder_traversal()
+tree = BinaryTree(10) #Inilializing root node
+tree.addNode(20) #will be inserted in right
+tree.addNode(5)  #will be inserted in left        
+tree.addNode(30)
+tree.addNode(40)
+tree.addNode(4)
+tree.addNode(3)
+
+#Traversal
+#Left-root-right
+print("\n**Inorder Traversal**")
+tree.inorder_traversal()
+
+#preorder
+print("\n**ُPreorder Traversal**")
+tree.preorder_traversal()

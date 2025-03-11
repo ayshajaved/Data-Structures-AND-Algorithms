744278767c0252fb6dec1d7ff06379da58e6c661
ayshajaved <ayshajavd@gmail.com>
2025-03-11 22:45:37 +0500

    Traversals in Binary tree

--- ClassNotes/7_Trees/3_BinarytreeCode.py
+++ ClassNotes/7_Trees/3_BinarytreeCode.py
@@ -3,6 +3,9 @@ Nodes are created for binary tree having data, left and right parameters.
 - Root pointer is maintained like head in linkedlist
 Operations:
 node creation, insertion, traversal (inorder, preorder, postorder), and searching.
+- Inorder (left--node--right)
+- postorder (Left, Right, Root)
+- preorder (Root, Left, Right)
 '''
 #Tree Node is defined for the binary trees
 class treeNode:
@@ -10,8 +13,10 @@ class treeNode:
         self.data = value
         self.left = None
         self.right = None
+
 class BinaryTree:
     def __init__(self, root_value):
         self.root = treeNode(root_value)
-        
     #method to insert the node
+    def addNode(self,value):
+        pass

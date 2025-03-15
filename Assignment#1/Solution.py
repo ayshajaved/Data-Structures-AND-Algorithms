3d6d7bdf8b1484ce6c4a8cb20907a427891c1973
ayshajaved <ayshajavd@gmail.com>
2025-03-15 21:09:27 +0500

    Create cycle method added

--- Assignment#1/Solution.py
+++ Assignment#1/Solution.py
@@ -7,6 +7,9 @@ class Floyd:
     def __init__(self):
         self.head = None
 
+    def getHead(self):
+        return self.head
+    
     #Method to add node in a single Linked List
     def appendNode(self, value):
         node = ListNode(value)
@@ -20,12 +23,55 @@ class Floyd:
             currentNode = currentNode.next
         currentNode.next = node
     
-    #Method to display the list
-    def printList(self):
+    #creating cycle
+    def createCycle(self, value):
+        current = self.head 
+        while current is not None and current.data !=value:
+            current = current.next
+        node = current
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = node
+
+    #Detect cycle
+    def detect_cycle(self):
+        #Using Floyd's Algorithm of two pointers
         if not self.head:
+            print("No list to detect cycle..")
+            return
+        #Two pointers
+        slow = self.head
+        fast = self.head
+        cycle_found = False
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow ==fast:
+                cycle_found = True
+                break
+        
+        if not cycle_found:
+            return self.head
+        slow = self.head
+        while slow !=fast:
+            slow = slow.next
+            fast = fast.next
+
+        #both slow and fast are at the start
+        cycle_start = slow
+        return cycle_start
+    
+    def remove_cycle(self, cycle_start):
+        pass
+
+    #Method to display the list
+    def printList(self, head):
+        if not head:
             print("List is empty..")
             return
-        currentNode = self.head
+        currentNode = head
         result = []
         while currentNode:
             result.append(str(currentNode.data))
@@ -39,6 +85,16 @@ list.appendNode(20)
 list.appendNode(30)
 list.appendNode(40)
 list.appendNode(50)
-list.printList()
+list.appendNode(60)
+list.appendNode(70)
+list.appendNode(80)
 
+#Creating cycle
+list.createCycle(40) #creates cycle of 80 --> 40
 
+#Printing list with cycle
+list.printList(list.getHead())
+
+#Detecting cycle
+cycle_start = list.detect_cycle()
+list.printList(cycle_start) #must print from 40
